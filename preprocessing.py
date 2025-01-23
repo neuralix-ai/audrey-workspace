6b4352535c2ee572b34f0888cbf2d5c041e3c2d0
Audrey Der <audrey@neuralix.ai>
2025-01-23 10:56:39 -0800

    Refactoring as Stage 1 Cleanup -- moving approved functions to files and out of working ipynb

--- preprocessing.py
+++ preprocessing.py
@@ -2,6 +2,8 @@ import os
 import pandas as pd
 
 
+# =======================================================================================
+
 
 # Code originally from Ryan Mercer; ported (01/15/2025) and cleaned here
 # Function to process voltage and current columns
@@ -51,41 +53,3 @@ def threshold_filtering(df):
 
     df[frequency_int_column] = df[frequency_float_column].round().astype('Int64')
     return df
-
-
-# def threshold_filtering(df):
-#     # If you need to ensure 'timestamp' is a datetime, reassign the index:
-#     df.index = pd.MultiIndex.from_arrays(
-#         [
-#             df.index.get_level_values('site_id'),
-#             df.index.get_level_values('pump_id'),
-#             pd.to_datetime(df.index.get_level_values('timestamp'))
-#         ],
-#         names=['site_id', 'pump_id', 'timestamp']
-#     )
-
-#     # If you need a date filter, you can do something like:
-#     # start_date = '2024-10-10'
-#     # end_date = df.index.get_level_values('timestamp').max()
-#     # df = df.loc[(slice(None), slice(None), slice(start_date, end_date)), :]
-
-#     # Define pump-related variables
-#     pressure_column = 'discharge pressure'
-#     flow_column = 'flow rate'
-#     frequency_float_column = 'frequency'
-#     frequency_int_column = 'frequency int'
-
-#     freq_min = 40
-#     freq_max = 60
-
-#     # Apply filtering masks on the main DataFrame (df)
-#     mask = (df[frequency_float_column].astype(float) >= freq_min) & (df[frequency_float_column].astype(float) <= freq_max)
-#     mask &= df[flow_column].astype(float) < 30000
-#     mask &= df[flow_column].astype(float) > 5000
-#     # If needed:
-#     # mask &= df[pressure_column].astype(float) > 1300
-
-#     df = df[mask]
-
-#     df[frequency_int_column] = df[frequency_float_column].round().astype('Int64')
-#     return df
