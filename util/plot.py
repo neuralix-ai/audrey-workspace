01a04185cbef56694790f2566406123f64cc2a60
Audrey Der <audrey@neuralix.ai>
2025-01-24 15:58:12 -0800

    QOL Plot Changes

--- util/plot.py
+++ util/plot.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
+from copy import deepcopy
 import numpy as np
 
-def plot_ts_gt(sitegts, freq_data, site_id):
+def plot_ts_gt(sitegts, freq_data_, site_id, site_bounds):
     # plot the time series estimated ground truth
     # the first plot is the time series and estiamted ground truth
     # the second plot is a set of surrounding indices before, during, and after the estimated ground truth for visual clarity
@@ -14,14 +15,17 @@ def plot_ts_gt(sitegts, freq_data, site_id):
     # ================================================
     
     # this is as many different colors you can assign to frequencies in calibration; add more as needed
+    freq_data = deepcopy(freq_data_)
+    freq_data.replace(0, np.nan, inplace=True)
     colors = ['c','m','y','r','g','b','lime','violet']
     site_gt = sitegts[site_id]
     fig = plt.figure(figsize=(30,4))
-    plt.plot(np.arange(len(freq_data)), freq_data,label="Frequency TS")
+    plt.plot(np.arange(site_bounds[0],site_bounds[1]), freq_data[site_bounds[0]:site_bounds[1]],label="Frequency TS")
     for i, freq in enumerate(site_gt.keys()):
         curr_start = site_gt[freq][0]
         curr_end = site_gt[freq][1]
-        plt.plot(np.arange(curr_start,curr_end), freq_data[curr_start:curr_end], c=colors[i])
+        plt.plot(np.arange(curr_start,curr_end), freq_data[curr_start:curr_end], c=colors[i], label="{} Hz".format(freq))
+    plt.legend()
     plt.grid()
     plt.show()
 
@@ -36,7 +40,8 @@ def plot_ts_gt(sitegts, freq_data, site_id):
     for i, freq in enumerate(site_gt.keys()):
         curr_start = site_gt[freq][0]
         curr_end = site_gt[freq][1]
-        plt.plot(np.arange(curr_start,curr_end), freq_data[curr_start:curr_end], c=colors[i])
+        plt.scatter(np.arange(curr_start,curr_end), freq_data[curr_start:curr_end], c=colors[i], label="{} Hz".format(freq))
+    plt.legend()
     plt.grid()
     plt.show()
     return
